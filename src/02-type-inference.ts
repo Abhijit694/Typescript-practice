42a5f885998d302e2bedb64a6eee82e96eb95f43
Abhijit694 <nayakabhijit543@gmail.com>
2026-02-16 16:41:28 +0530

    type interface in ts

---
+++ src/02-type-inference.ts
@@ -0,0 +1,3 @@
+// Type inference   ->  TypeScript's ability to automatically assign a type to a variable based on its initial value without explicit type annotations  
+
+let inferredString = "This is a string"
