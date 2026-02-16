2676d97dfdb823a3979cf4dc92c8aa6f5daa93bd
Abhijit694 <nayakabhijit543@gmail.com>
2026-02-16 23:48:31 +0530

    union and intersections in typescript

---
+++ src/06-union-intersection.ts
@@ -0,0 +1,24 @@
+// Union types (OR)
+type Status = "pending" | "approved" | "rejected" | boolean
+
+function setStatus(status: Status):void{
+    console.log(`Status set to : ${status}`);
+}
+setStatus("approved")
+
+
+// Intersection (AND)
+interface Colorful {
+    color: string
+}
+
+interface Circle {
+    radius: number
+}
+
+type ColorfulCircle = Colorful & Circle
+
+let myCircle: ColorfulCircle = {
+    color: "red",
+    radius: 10
+}
